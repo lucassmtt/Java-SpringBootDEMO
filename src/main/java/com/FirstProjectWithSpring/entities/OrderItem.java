d0b5d0b3e1b516f681e354b51dcd7ce127914948
Lucas Motta <110114441+lucassmtt@users.noreply.github.com>
2023-08-03 08:25:30 -0300

    Subtotal e Total methods...

--- src/main/java/com/FirstProjectWithSpring/entities/OrderItem.java
+++ src/main/java/com/FirstProjectWithSpring/entities/OrderItem.java
@@ -30,6 +30,10 @@ public class OrderItem implements Serializable {
         this.price = price;
     }
 
+    public Double getSubTotal() {
+        return quantity * price;
+    }
+
     @JsonIgnore
     public Order getOrder() {
         return id.getOrder();
